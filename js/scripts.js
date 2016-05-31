997b071f403f084edfb617b9ed470a0031ec0942
Adam Luchini and Katarina Karolcikova <katarina@>
2016-05-31 11:07:50 -0700

    calculator operations

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,36 @@
+var sub = function(number1, number2) {
+  return number1 - number2;
+};
 var add = function(number1, number2) {
   return number1 + number2;
 };
 
+var substract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+
 var number1 = parseInt(prompt("Enter your number:"));
 var number2 = parseInt(prompt("Enter another number:"));
+var result = sub(number1, number2);
+alert (result);
 
-alert(add(number1, number2));
+
+
+
+var weight = parseInt(prompt("Enter your weight in lbs:"));
+var height = parseInt(prompt("Enter your height in inches:"));
+var result = bmi(weight, height);
+alert (result);
+
+var bmi = function (weight, height){
+	return ((weight * 703) / (height * height));
+};
