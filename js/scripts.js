52810d52be97845092c119303f27ca47bc5c526d
Julia Dickey and Katarina Karolcikova <katarina@>
2016-06-01 10:15:58 -0700

    Started integrating UI and business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,46 @@
+
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var substract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+
+var number1 = parseInt(prompt("Enter your number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+
+alert (add(number1, number2));
+
+
+// var weight = parseInt(prompt("Enter your weight in lbs:"));
+// var height = parseInt(prompt("Enter your height in inches:"));
+// var result = bmi(weight, height);
+// alert (result);
+//
+// var bmi = function (weight, height){
+// 	return ((weight * 703) / (height * height));
+// };
+//
+// var fahren = parseInt(prompt("What is the temp in F?"));
+// var result = ((fahren - 32)*(5/9));
+// alert (result);
+//
+// var gal = parseInt(prompt("What is the temp in gallons?"));
+// var result = (gal/4) ;
+// alert (result);
 // var sub = function(number1, number2) {
 //   return number1 - number2;
 // };
-// var add = function(number1, number2) {
-//   return number1 + number2;
-// };
-//
-// var substract = function(number1, number2) {
+// var sub = function(number1, number2) {
 //   return number1 - number2;
 // };
-//
-// var multiply = function(number1, number2) {
-//   return number1 * number2;
-// };
-//
-// var divide = function(number1, number2) {
-//   return number1 / number2;
-// };
-//
-//
-// var number1 = parseInt(prompt("Enter your number:"));
-// var number2 = parseInt(prompt("Enter another number:"));
-// var result = sub(number1, number2);
-// alert (result);
-
-
-var weight = parseInt(prompt("Enter your weight in lbs:"));
-var height = parseInt(prompt("Enter your height in inches:"));
-var result = bmi(weight, height);
-alert (result);
-
-var bmi = function (weight, height){
-	return ((weight * 703) / (height * height));
-};
-
-var fahren = parseInt(prompt("What is the temp in F?"));
-var result = ((fahren - 32)*(5/9));
-alert (result);
-
-var gal = parseInt(prompt("What is the temp in gallons?"));
-var result = (gal/4) ;
-alert (result);
