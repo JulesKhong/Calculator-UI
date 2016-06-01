9011368974765303494b455a8e1561d690776391
Julia Dickey and Katarina Karolcikova <julia@>
2016-06-01 11:11:32 -0700

    Updated script to complete addition

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//Business or back-end logic:
 
 var add = function(number1, number2) {
   return number1 + number2;
@@ -15,11 +16,16 @@ var divide = function(number1, number2) {
   return number1 / number2;
 };
 
-
-var number1 = parseInt(prompt("Enter your number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-
-alert (add(number1, number2));
+// User interface or front-end logic:
+$(document).ready(function() {
+	$("form#add").submit(function(event) {
+		event.preventDefault();
+		var number1 = parseInt($("#add1").val());
+		var number2 = parseInt($("#add2").val());
+		var result = add(number1, number2);
+		$("#output").text(result);
+	});
+});
 
 
 // var weight = parseInt(prompt("Enter your weight in lbs:"));
